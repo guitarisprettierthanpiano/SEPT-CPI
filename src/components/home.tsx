33d1f66a6aa586480f0830d7e19ef4a4c28ac35b
guitarisprettierthanpiano <shfabrizio@gmail.com>
2021-10-16 06:05:26 -0400

    added home page component

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 //api key 752661a6e5f2a42fdee48e14e68a69eb
 
 const Home: React.FC = () => {
-    http://api.aviationstack.com/v1/flights?access_key=752661a6e5f2a42fdee48e14e68a69eb
+    http://api.aviationstack.com/v1/airports?access_key=752661a6e5f2a42fdee48e14e68a69eb
+
 
     return(
         <h3>home</h3>
