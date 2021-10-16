7a0844996eb8a1abd2f0d40eeba4f0d147c29415
guitarisprettierthanpiano <shfabrizio@gmail.com>
2021-10-16 05:47:19 -0400

    added the bundle.js and set up index.html

---
+++ src/app.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+import Home from './components/home'
+
+const App: React.FC = () => {
+
+    return(
+        <>
+        <div className='testing'>
+            <h1>HIIIreter</h1>
+        </div>
+
+        <Home />
+        </>
+    )
+}
+
+export default App;
---
+++ src/components/home.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+//api key 752661a6e5f2a42fdee48e14e68a69eb
+
+const Home: React.FC = () => {
+    http://api.aviationstack.com/v1/flights?access_key=752661a6e5f2a42fdee48e14e68a69eb
+
+    return(
+        <h3>home</h3>
+    )
+}
+
+export default Home;
